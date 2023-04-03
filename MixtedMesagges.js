04a2a2765fa2a4754ab89e375946ce7d67c3c3ed
johncastal <fredcastal@outlook.com>
2023-04-03 10:40:43 -0500

    first commit proyect

---
+++ MixtedMesagges.js
@@ -0,0 +1,28 @@
+const random = (limit) => Math.floor(Math.random() * (limit));
+
+const quote = () => {
+
+    const Message = {
+        'date' : ['Today','This week', 'In your weekend','Everyday','In the morning','In the evening','In noon','Every Monday'],
+        'messages' : ['Aim high','Have faith','Be awesome','Accept yourself','Alive & well','Act justly','Amplify hope'],
+        'complement' : ['Read','Hike', 'Cook Something New', 'Try A New Workout On YouTube','Take A Hot Bath','Discover New Music',"Find What You're Grateful For"]
+    }
+    
+    output = [];
+    for(let m in Message){
+        if(m==='date'){
+            output.push(Message[m][random(Message[m].length)]);
+            output.push(["in your life,"]);
+        }else if(m==='messages'){
+            output.push(["think how --"]);
+            output.push(Message[m][random(Message[m].length)]);
+            output.push(["--"]);
+        }else if(m==='complement'){
+            output.push(["and don't forget..."]);
+            output.push(Message[m][random(Message[m].length)]);
+        }              
+    }
+    return output.join(' ')
+}
+
+console.log(quote())
